c231bb4a40ab59d485503cf8404803574a0889eb
Eriqeu <ndwigaheric12@gmail.com>
2022-09-08 12:23:29 -0700

    whueee

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - Prunts a text
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write(2, "and thatvpiece of art os useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Printing
+ *
+ * Return: Always 0 (Succesful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multulingual puzzle");
+	return (0);
+}
