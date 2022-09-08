4c6a3e5e9a2fbac145e48e5e2a7f3aae63ff11b4
Eriqeu <ndwigaheric12@gmail.com>
2022-09-08 12:32:24 -0700

    whueee

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Printing
+ * main - ptint the string in the put function
  *
- * Return: Always 0 (Succesful)
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
  */
 int main(void)
 {
